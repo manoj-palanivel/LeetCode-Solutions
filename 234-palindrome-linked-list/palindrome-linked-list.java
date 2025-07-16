2f4d6a694ef923c1a2f82dde8a25258e923a267f
manoj-palanivel <160488384+manoj-palanivel@users.noreply.github.com>
2025-07-16 13:35:37 +0530

    Time: 15 ms (9.26%) | Memory: 58.7 MB (75.45%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,43 +9,19 @@
  * }
  */
 class Solution {
-    public static ListNode reverse(ListNode d){
-        if (d == null || d.next == null) {
-            return d;
-        }
-        ListNode newHead = reverse(d.next);
-        d.next.next = d;
-        d.next = null;
-        return newHead;
-    }
-
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-
-        // Find the middle of the linked list
-        ListNode fast = head;
-        ListNode slow = head;
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-
-        // Reverse the second half
-        ListNode revhead = reverse(slow.next);
-
-        // Compare both halves
-        ListNode firstHalf = head;
-        ListNode secondHalf = revhead;
-        while (secondHalf != null) {
-            if (firstHalf.val != secondHalf.val) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
+       Stack<Integer>st=new Stack<>();
+       ListNode temp=head;
+       while(temp!=null){
+        st.push(temp.val);
+        temp=temp.next;
+       }
+       temp=head;
+       while(!st.isEmpty() && temp!=null){
+        int x=(int)st.pop();
+        if(x!=temp.val) return false;
+        temp=temp.next;
+       }
+       return true;
     }
 }
