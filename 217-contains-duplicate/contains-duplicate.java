88045d58aa78e7e2d293cf7e70e10abfc4dc4854
manoj-palanivel <160488384+manoj-palanivel@users.noreply.github.com>
2025-09-14 13:27:45 +0530

    Time: 21 ms (20.06%) | Memory: 58.3 MB (62.18%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int a = 0;
+
+        for(int i=0;i<nums.length - 1;i++){
+            if(nums[i] == nums[i+1]){
+                a++;
+            }
+            else{
+                continue;
+            }
+        }
+        if(a>0){
+            return true;
+        }
+        return false;
+    }
+}
